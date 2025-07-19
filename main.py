4387af7728cfd14681281e7466762a48e0d7b985
Ivan Sokhin <SokhinIvan@yandex.ru>
2025-07-19 18:42:31 +0300

    add video stream

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        cam, frame = cap.read()
+
+        cv2.imshow("cam", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
